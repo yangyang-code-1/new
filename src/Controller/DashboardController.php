d39a2bba27d96ccf959444fc5c96c2668c008baf
yangyang-code-1 <sgtagura@gmail.com>
2025-11-11 20:43:16 +0800

    Add files via upload

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        return new Response('<h1>Welcome to the Dashboard! (Protected Area)');
+    }
+}
